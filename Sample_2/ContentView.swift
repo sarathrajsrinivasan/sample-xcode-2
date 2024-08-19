0262dec4111311c66c52dbec3d856c79a210251a
Sarathraj Srinivasan <sarathrajsrinivasan@github.com>
2024-08-19 18:42:58 -0500

    first commit

---
+++ Sample_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sample_2
+//
+//  Created by Sarathraj Srinivasan on 8/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sample_2/Sample_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_2App.swift
+//  Sample_2
+//
+//  Created by Sarathraj Srinivasan on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Sample_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
